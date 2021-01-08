7ae0db4fbfc3d6134c20cf84c3d0d57a66fdd53d
Hong Seung-Gi <tmdrl5661@naver.com>
2021-01-08 19:18:35 +0900

    [Add] 21.01.08 두 정수 사이의 합

---
+++ 21.01.08_문제풀이/21.01.08_NumberSum.js
@@ -0,0 +1,19 @@
+/*
+두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+ */
+function solution(a, b) {
+    var answer = 0;
+    var small = () => {
+        var e = 0
+        return e = a < b ? a : b
+    };
+    var big = () => {
+        var e = 0
+        return e = a > b ? a : b
+    };
+    for(let i = small(); i <= big(); i++){
+        answer += i;
+    }
+    return answer;
+}
